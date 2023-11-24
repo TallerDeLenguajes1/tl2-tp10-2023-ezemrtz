3aa11deee90cf9232b113176287884596656dc04
Ezequiel <ezequielmartinez2424@gmail.com>
2023-11-24 19:24:14 -0300

    Avances tp11

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,10 +1,17 @@
 namespace tl2_tp10_2023_ezemrtz;
 
-
+public enum NivelAcceso{
+    administrador = 1,
+    operador = 2
+}
 public class Usuario{
     private int id;
     private string nombreDeUsuario;
+    private string contrasenia;
+    private NivelAcceso nivelDeAcceso;
 
     public int Id { get => id; set => id = value; }
     public string NombreDeUsuario { get => nombreDeUsuario; set => nombreDeUsuario = value; }
+    public string Contrasenia { get => contrasenia; set => contrasenia = value; }
+    public NivelAcceso NivelDeAcceso { get => nivelDeAcceso; set => nivelDeAcceso = value; }
 }
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace tl2_tp10_2023_ezemrtz.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [Display(Name = "Nombre de Usuario")] 
+        public string Nombre {get;set;}        
+        
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [PasswordPropertyText]
+        [Display(Name = "Contraseña")]
+        public string Contrasenia {get;set;}
+    }
+}
